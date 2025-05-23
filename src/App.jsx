178c940aea1b777311dc25985a434595221cace6
DevanshuTiwaskar <devanshutiwaskar@gmail.com>
2025-05-23 23:02:55 +0530

    feat: Initialize project structure with routing and context for recipes
    - Added main application component (App.jsx) with initial routing setup.
    - Implemented navigation component (Nav.jsx) with links to Home, Recipes, About, and Favorite pages.
    - Created pages for Home, Recipes, About, Create, Favorite, Update, and a 404 Page Not Found.
    - Established RecipeContext for managing recipe data across components.
    - Integrated Tailwind CSS for styling and responsive design.
    - Added SVG logo for branding.
    - Configured Vite for React and Tailwind CSS support.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import MainRoutes from "./routes/Mainroutes";
+import Nav from "./components/Nav";
+import GetStart from "./pages/GetStart";
+const App = () => {
+    return (
+        <div>
+            {/* <Nav /> */}
+            {/* <MainRoutes /> */}
+            <GetStart/>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/pages/GetStart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GetStart = () => {
+  return (
+    <div className='w-full h-min-screen flex flex-col items-center justify-center'>
+      <img src="../../public/getStartPhoto.jpg" className="w-full h-[] rounded-lg mt-30" alt="Dish" />
+
+
+    </div>
+  )
+}
+
+export default GetStart
